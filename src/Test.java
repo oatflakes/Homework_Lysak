98fdd3bfa9d4aa21e0fee5cd673e1916084e3c9a
Oakflakes <Polina1811>
2017-06-15 20:02:54 +0300

    готово

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * Created by DL on 13-Jun-17.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            int sum = 0;
+            sum = sum + i;
+            System.out.println("Cyммa: " + sum);
+
+        }
+    }
+}
+
+
